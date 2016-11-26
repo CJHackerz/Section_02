998ec9d1327b9fd95ef25d21af901abde13e7dca
Chirag Jariwala <cjhackerz@yahoo.com>
2016-11-26 15:38:15 +0530

    Function GetGuess
    Defined a function to get guess from user and print it back to console.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -2,30 +2,35 @@
 #include <string>
 using namespace std;
 
-//Functions
+//Function prototypes
 void PrintIntro(); 
+string GetGuess();
 
 int main()
 {
 	//Function to print game intro.
 	PrintIntro();
-	
-	//get guess from the player.
-	string Guess = "";
-	cout << "Enter your Guess: ";
-	getline(cin, Guess);
-	//repeat guess back to them.
-	cout << "Your guess was: " << Guess << endl;
-	
+
+	//Function to get guess from user and print it back
+	GetGuess();
 	
 	return 0;
 }
 
 //Function Definations.
-void PrintIntro() {
+void PrintIntro() { //introduce the game
 	//Game Introduction.
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'am thinking of?" << endl;
+}
+
+string GetGuess() { //get a guess from a player.
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	//Print it back.
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
 }
